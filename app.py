028a861d19d03da613377847539de04215dfea91
Felipe Pontes <felipemfpontes@gmail.com>
2017-02-17 11:12:00 -0300

    Make points clickable and show profile (dataanalysis)

--- app.py
+++ app.py
@@ -190,4 +190,4 @@ def runiuga():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8000))
-    app.run(host='0.0.0.0', use_reloader=True, port=port)
+    app.run(use_reloader=True, port=port)
