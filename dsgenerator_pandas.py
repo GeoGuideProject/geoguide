385c1f09a7728987018ea3f8524ac17a367c0a9d
Felipe Pontes <felipemfpontes@gmail.com>
2017-02-21 15:23:05 -0300

    Update homepage

--- dsgenerator_pandas.py
+++ dsgenerator_pandas.py
@@ -13,15 +13,15 @@ def harvestine_distance(lat1, lng1, lat2, lng2):
         d = math.acos(math.cos(dept_lat_rad) * math.cos(dept_lng_rad) * math.cos(arr_lat_rad) * math.cos(arr_lng_rad) + math.cos(dept_lat_rad) *
                       math.sin(dept_lng_rad) * math.cos(arr_lat_rad) * math.sin(arr_lng_rad) + math.sin(dept_lat_rad) * math.sin(arr_lat_rad)) * earth_radius
         return round(d, 2)
-    except:
+    except ValueError:
         return 0
 
-df = pd.read_csv('tmp/arquivo.csv')
+df = pd.read_csv('arquivo_bike.csv')
 
-df = df[((40.495730 < df['start station latitude']) & (df['start station latitude'] < 40.915031)) &
-        ((40.495730 < df['end station latitude']) & (df['end station latitude'] < 40.915031)) &
-        ((-74.255698 < df['end station longitude']) & (df['end station longitude'] < -73.700208)) &
-        ((-74.255698 < df['start station longitude']) & (df['start station longitude'] < -73.700208))]
+# df = df[((40.495730 < df['start station latitude']) & (df['start station latitude'] < 40.915031)) &
+#         ((40.495730 < df['end station latitude']) & (df['end station latitude'] < 40.915031)) &
+#         ((-74.255698 < df['end station longitude']) & (df['end station longitude'] < -73.700208)) &
+#         ((-74.255698 < df['start station longitude']) & (df['start station longitude'] < -73.700208))]
 
 df['starttime'] = df['starttime'].apply(
     lambda x: arrow.get(x, 'M/D/YYYY HH:mm:ss').isoformat())
@@ -58,7 +58,6 @@ df = df.append(df_ends, ignore_index=True)
 
 del df_ends
 
-# df.to_csv('pandas.index.csv', index_label='id')
 df.to_hdf('pandas.store.h5', 'index', format='fixed')
 
 bigger_distance = 0
@@ -69,7 +68,6 @@ x = 1
 n = len(df)
 
 for row_a in df.itertuples():
-    # print('Left:', n - x)
     for row_b in df_aux.loc[x:].itertuples():
         distance = harvestine_distance(row_a[1], row_a[2],
                                        row_b[1], row_b[2])
@@ -87,5 +85,4 @@ df_ds = pd.DataFrame(
     similarity=lambda x: x.similarity / bigger_distance
 )
 
-# df_ds.to_csv('pandas.ds.csv', index=False)
 df_ds.to_hdf('pandas.store.h5', 'ds', format='fixed')
