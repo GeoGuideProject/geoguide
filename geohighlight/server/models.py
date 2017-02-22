9a75fc2645c7067338008dc646cd4501b65b22dc
Felipe Pontes <felipemfpontes@gmail.com>
2017-02-22 10:39:14 -0300

    Update upload UI

--- geohighlight/server/models.py
+++ geohighlight/server/models.py
@@ -9,12 +9,17 @@ class Dataset(db.Model):
     __tablename__ = 'datasets'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(100), nullable=False)
+    number_of_rows = db.Column(db.Integer)
     filename = db.Column(db.String(40), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False)
 
 
-    def __init__(self, filename):
+    def __init__(self, title, filename, number_of_rows=None):
+        self.title = title
         self.filename = filename
+        if number_of_rows is not None:
+            self.number_of_rows = number_of_rows
         self.created_at = datetime.datetime.now()
 
 
